4eb35ba2d29352ce10a71b1df055543f87b685cb
Biggy <jululien.gautier@laposte.net>
2020-01-15 17:14:56 +0100

    Ajoutez des fichiers projet.

---
+++ APITest/Context/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITest.Context
+{
+    public class Test
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ APITest/Data/APITestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APITest.Context;
+
+namespace APITest.Models
+{
+    public class APITestContext : DbContext
+    {
+        public APITestContext (DbContextOptions<APITestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APITest.Context.Test> Test { get; set; }
+    }
+}
